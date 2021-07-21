9ca65472b4c987efc5c35b928faddc72c0a8bbf8
html2021-1 <58695492+html2021-1@daum.net>
2021-07-21 22:48:40 +0900

    gnb, 본문2 스크립트

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  // 메뉴열기 클릭
+  $('.nav .btn_open').on('click', function () {
+    $(this).next().css({visibility: 'visible'}).stop().animate({right: 0}, function () {
+      $(this).find('a').first().focus();
+    });
+  });
+
+  // 메뉴에서 닫기를 누르기 전에는 포커스가 외부로 나가지 못하게 제한
+  $('#gnb a').first().on('keydown', function (e) {
+    const key = e.keyCode;
+    if (e.shiftKey && key === 9) {
+      e.preventDefault();
+      $('.nav .btn_close').focus();
+    }
+  });
+  $('#gnb button').on('keydown', function (e) {
+    const key = e.keyCode;
+    if (!e.shiftKey && key === 9) {
+      e.preventDefault();
+      $('#gnb a').first().focus();
+    }
+  });
+
+
+  // 메뉴 닫기 클릭
+  $('.nav .btn_close').on('click', function () {
+    $(this).parent().stop().animate({right: '-22%'}, function () {
+      $(this).css({visibility: 'hidden'});
+      $('.nav .btn_open').focus();
+    });
+  });
+});
---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  // 본문2 초기 설정
+  $('#content2 .pagenation li:first').addClass('on');
+  $('#content2 .cnt2_txt > .txt1').addClass('selected');
+  $('#content2 .bg div:first').addClass('view');
+
+  // 본문2 오른쪽 페이지 네이션 클릭
+  $('#content2 .pagenation button').on('click', function () {
+    const tgNum = $(this).parent().index();
+    
+    $(this).parent().addClass('on').siblings().removeClass('on');
+    $('#content2 .cnt2_txt > div').eq(tgNum).addClass('selected').siblings().removeClass('selected');
+    $('#content2 .bg div').eq(tgNum).addClass('view').siblings().removeClass('view');
+  });
+
+  $(window).on('scroll', function () {
+    const scrollY = $(window).scrollTop();
+    const cnt2Y = $('#content2').offset().top;
+    const cnt2Hei = $(this).height();
+
+    // 본문2에서 스크롤 될때 : 본문2의 시작에서는 .txt1을 활성화 / 50픽셀 이상 움직이면 .txt2를 활성화
+    if (scrollY >= cnt2Y && scrollY < cnt2Y + 50) {
+      $('#content2 .pagenation li').eq(0).children().click();
+    }
+    else if (scrollY >= cnt2Y + 50 && scrollY < cnt2Y + cnt2Hei) {
+      $('#content2 .pagenation li').eq(1).children().click();
+    } 
+  });
+});
